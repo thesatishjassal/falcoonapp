ef190451204e62453544efdf0b8b0f79629f5ccd
Satish jassal <thesatishjassal@gmail.com>
2026-05-06 17:34:05 +0530

    swith and quotaion UI

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -135,7 +135,7 @@ export default function Hero({ onOpenModal }) {
           </div>
 
           <img
-            src="/assets/images/falcoon_hero-image-v2.png"
+            src="/assets/images/falcoon_hero-image.png"
             alt="Falcoon Funnel Visual"
           />
         </div>
