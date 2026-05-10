ac8f4efa4a9d40a7e98c39e62d58f7bf74b70f6c
Satish Jassal <thesatishjassal@gmail.com>
2026-05-11 01:28:22 +0530

    first commit

--- src/app/components/supportfloat.jsx
+++ src/app/components/supportfloat.jsx
@@ -7,40 +7,28 @@ export default function SupportFloat() {
 
   return (
     <div className="falcoon-support">
-      
       {/* MAIN BUTTON */}
-      <button
-        className="falcoon-support__main"
-        onClick={() => setOpen(!open)}
-      >
+      <button className="falcoon-support__main" onClick={() => setOpen(!open)}>
         💬
       </button>
 
       {/* OPTIONS */}
       <div className={`falcoon-support__menu ${open ? "active" : ""}`}>
-        
         <a
-          href="https://wa.me/919999999999"
+          href="https://wa.me/+91 7888467258"
           target="_blank"
           className="falcoon-support__item whatsapp"
         >
           <span>WhatsApp Chat</span>
         </a>
 
-        <a
-          href="tel:+919999999999"
-          className="falcoon-support__item call"
-        >
+        <a href="tel:++91 7888467258" className="falcoon-support__item call">
           <span>Call Us</span>
         </a>
 
-        <a
-          href="/faqpage"
-          className="falcoon-support__item help"
-        >
+        <a href="/faqpage" className="falcoon-support__item help">
           <span>Help Center</span>
         </a>
-
       </div>
     </div>
   );
